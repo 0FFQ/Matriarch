5df03c55e4c44ca33fcfe4ce59bb1a6058322979
0FFQ <denisov468968@mail.ru>
2026-04-15 21:07:07 +0300

    Upgrade: убрал у кнопки messenger-fab анимацию появления

--- src/components/messenger/MessengerButton.js
+++ src/components/messenger/MessengerButton.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import { motion } from 'framer-motion';
 import { MessageCircle } from 'lucide-react';
 
 const MessengerButton = ({ onClick, unreadCount }) => {
   return (
-    <motion.button
+    <button
       className="messenger-fab"
-      initial={{ scale: 0, opacity: 0 }}
-      animate={{ scale: 1, opacity: 1 }}
       onClick={onClick}
     >
       <MessageCircle size={20} />
       {unreadCount > 0 && (
-        <motion.span
+        <span
           className="messenger-fab-badge"
-          initial={{ scale: 0 }}
-          animate={{ scale: 1 }}
         >
           {unreadCount > 99 ? '99+' : unreadCount}
-        </motion.span>
+        </span>
       )}
-    </motion.button>
+    </button>
   );
 };
 
